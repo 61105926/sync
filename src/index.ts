99034102c8e0f9358b7f68b810f750fdfb511336
erwinchoquec <erwinchoque@hotmail.com>
2026-03-09 16:40:46 -0400

    d/v3

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,10 @@ import './jobs/scheduler';
 
 const port = env.port;
 
-app.listen(port, () => {
-  console.log(`Servidor ejecutándose en http://localhost:${port}`);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Servidor ejecutándose en http://0.0.0.0:${port}`);
   console.log(`Keycloak: ${env.keycloak.serverUrl}`);
   console.log(`Realm: ${env.keycloak.realm}`);
   console.log(`Cliente: ${env.keycloak.clientId}`);
-  console.log(`API: POST http://localhost:${port}/change-password { username, currentPassword, newPassword }`);
+  console.log(`API: POST http://0.0.0.0:${port}/change-password`);
 });
